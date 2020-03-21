ee361c035e40299192531781e7d05ebafa22d519
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2020-03-21 16:42:08 +0000

    Automated build from Jenkins

---
+++ SDL2/SDL2_framerate.h
@@ -0,0 +1,10 @@
+
+/*
+
+SDL2_framerate.h: framerate manager
+
+Copyright (C) 2012  Andreas Schiffler
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
