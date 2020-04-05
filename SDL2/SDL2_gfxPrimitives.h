1266244d36290b5f0c32e026c0602fb16ed25a2c
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2020-04-05 21:58:04 +0000

    Some improvements.

--- SDL2/SDL2_gfxPrimitives.h
+++ SDL2/SDL2_gfxPrimitives.h
@@ -35,3 +35,10 @@ Andreas Schiffler -- aschiffler at ferzkopp dot net
 #ifndef M_PI
 #define M_PI	3.1415926535897932384626433832795
 #endif
+
+#include <SDL2/SDL.h>
+
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
