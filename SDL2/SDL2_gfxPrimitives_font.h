5360ccdf74d758dc443c083e90a791e36e1152f0
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2020-05-10 15:53:57 +0000

    Add CHANGELOG entry

--- SDL2/SDL2_gfxPrimitives_font.h
+++ SDL2/SDL2_gfxPrimitives_font.h
@@ -3,3 +3,15 @@
 /* ---- 8x8 font definition ---- */
 
 /*  ZLIB (c) A. Schiffler 2012 */
+
+#define GFX_FONTDATAMAX (8*256)
+
+static unsigned char gfxPrimitivesFontdata[GFX_FONTDATAMAX] = {
+
+	/*
+	* 0 0x00 '^@' 
+	*/
+	0x00,			/* 00000000 */
+	0x00,			/* 00000000 */
+	0x00,			/* 00000000 */
+	0x00,			/* 00000000 */
