a620eeb5df788b2b8fb498b551a913e0777f9058
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-06-09 22:40:29 +0000

    Bump dependency versions.

---
+++ SDL2/SDL2_imageFilter.h
@@ -0,0 +1,13 @@
+
+/*
+
+SDL2_imageFilter.h: byte-image "filter" routines 
+
+Copyright (C) 2001-2012  Andreas Schiffler
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
