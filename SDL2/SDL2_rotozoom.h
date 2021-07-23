d372359cce7a6b026ab896e6cfc959eb3ca5aba9
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-07-23 16:56:17 +0000

    look

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -5,4 +5,4 @@ SDL2_rotozoom.c: rotozoomer, zoomer and shrinker for 32bit or 8bit surfaces
 Copyright (C) 2001-2012  Andreas Schiffler
 
 This software is provided 'as-is', without any express or implied
-warranty. In no 
+warranty. In no event wil
