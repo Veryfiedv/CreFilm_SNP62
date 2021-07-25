26aa59cbe73e67c31cda59c82857eb26eae71f4d
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-07-25 20:45:18 +0000

    Add license to composer.json

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -6,4 +6,4 @@ Copyright (C) 2001-2012  Andreas Schiffler
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
-aris
+arising from t
