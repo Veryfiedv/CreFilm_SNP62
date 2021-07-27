b320bfa8925deb0a925ab3aaa948bfb6320c01d9
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-07-27 12:38:37 +0000

    fixes #4

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -6,4 +6,6 @@ Copyright (C) 2001-2012  Andreas Schiffler
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
-arising from the use of this 
+arising from the use of this software.
+
+Pe
