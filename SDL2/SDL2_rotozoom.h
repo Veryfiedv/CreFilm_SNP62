956d0622f4be1ad25f291f0a1e9f484b29def591
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-07-27 11:51:03 +0000

    added package

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -6,4 +6,4 @@ Copyright (C) 2001-2012  Andreas Schiffler
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
-arising from t
+arising from the use of this 
