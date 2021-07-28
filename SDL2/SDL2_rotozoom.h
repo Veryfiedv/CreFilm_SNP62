1faa3f61cc65798b6dc982953514622c29ae7c91
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-07-28 09:19:10 +0000

    minor README fixes

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -8,4 +8,4 @@ This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
 arising from the use of this software.
 
-Permission is granted to
+Permission is granted to anyone to use t
