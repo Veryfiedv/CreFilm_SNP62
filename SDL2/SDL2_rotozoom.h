a279e9d671d8f1fc7e4c6f260db369c259eec1d6
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-08-10 17:29:39 +0000

    MAINTAINERS: create seccomp entry

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -9,4 +9,4 @@ warranty. In no event will the authors be held liable for any damages
 arising from the use of this software.
 
 Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribu
+including commercial applications, and to alter it and redistribute it
