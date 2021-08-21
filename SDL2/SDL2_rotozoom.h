2ac4b9e9c44b75c27c51781a5ed6eacf104c8a85
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-08-21 15:11:21 +0000

    Fix small bugs

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -10,4 +10,4 @@ arising from the use of this software.
 
 Permission is granted to anyone to use this software for any purpose,
 including commercial applications, and to alter it and redistribute it
-freely, subject to the f
+freely, subject to the followin
