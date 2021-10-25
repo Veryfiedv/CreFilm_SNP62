efcf0c25c5ce58f4fb90da77c21f968c83fc28bc
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2021-10-25 16:43:53 +0000

    Pontoon: Update Frisian (fy-NL) localization of AMO

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -23,4 +23,7 @@ misrepresented as being the original software.
 3. This notice may not be removed or altered from any source
 distribution.
 
-Andreas Schiffler -- aschiffler at ferzko
+Andreas Schiffler -- aschiffler at ferzkopp dot net
+
+*/
+
