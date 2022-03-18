306bd109877181630b44e30434f67bf6bc54ad90
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2022-03-18 15:13:40 +0000

    Update chefignore

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -76,4 +76,4 @@ extern "C" {
 
 	*/
 
-	SDL2_ROTOZOOM_SCOPE SDL_Surface *rotozoomSurface(SDL_Surface * src, doubl
+	SDL2_ROTOZOOM_SCOPE SDL_Surface *rotozoomSurface(SDL_Surface * src, double angle, double
