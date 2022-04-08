ee667e525e977fa58a87cd32a421f2ec0e59db9e
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2022-04-08 21:15:11 +0000

    Added specs

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -79,4 +79,4 @@ extern "C" {
 	SDL2_ROTOZOOM_SCOPE SDL_Surface *rotozoomSurface(SDL_Surface * src, double angle, double zoom, int smooth);
 
 	SDL2_ROTOZOOM_SCOPE SDL_Surface *rotozoomSurfaceXY
-		(SDL_Surface * src, double angle, double zoomx, double zoomy, 
+		(SDL_Surface * src, double angle, double zoomx, double zoomy, int smooth
