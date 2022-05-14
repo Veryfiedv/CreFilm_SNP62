7834dea5a29866151a61da0038e795c475058fee
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2022-05-14 21:16:15 +0000

    typo;

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -86,4 +86,4 @@ extern "C" {
 		int *dstheight);
 
 	SDL2_ROTOZOOM_SCOPE void rotozoomSurfaceSizeXY
-		(int width, int height, double angle, doubl
+		(int width, int height, double angle, double zoomx, 
