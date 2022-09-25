7a743a978f76ef4dbf23aa9af815c73605236a8a
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2022-09-25 10:57:26 +0000

    style check

--- SDL2/SDL2_rotozoom.h
+++ SDL2/SDL2_rotozoom.h
@@ -116,4 +116,5 @@ extern "C" {
 	SDL2_ROTOZOOM_SCOPE SDL_Surface* rotateSurface90Degrees(SDL_Surface* src, int numClockwiseTurns);
 
 	/* Ends C function definitions when using C++ */
-#ifdef __c
+#ifdef __cplusplus
+}
