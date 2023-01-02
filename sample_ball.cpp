01266d9160313abc290fadce0e2dec0978e043b5
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-01-02 15:59:54 +0000

    Javadoc typo

--- sample_ball.cpp
+++ sample_ball.cpp
@@ -26,3 +26,21 @@ static const ColourRGB BALL_COLOURS[] = {
 	ColourRGB(255,0,0),
 	ColourRGB(0,255,0),
 	ColourRGB(0,0,255),
+	ColourRGB(0,255,255),
+	ColourRGB(255,0,255),
+	ColourRGB(238,238,0),
+	ColourRGB(204,153,225),
+	ColourRGB(204,204,255)
+
+};
+static float VELOCITY []={ 400,350,300,250,200,150,150};
+static const int BALL_RADIUS[]= {5,10,15,20,25,30};
+class A2Canvas{
+public:
+	static const int CANVAS_SIZE_X = 800;
+	static const int CANVAS_SIZE_Y = 600;
+	static const int UNIT_X = CANVAS_SIZE_X/16;
+	static const int UNIT_Y = CANVAS_SIZE_Y/20;
+	 //Pixels/second
+
+	static const int NUM_COLOURS = 7;
