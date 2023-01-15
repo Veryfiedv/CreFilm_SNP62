8217ee7e4ac4d440ccdc1c149ef768d035c95d71
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-01-15 08:45:37 +0000

    tmux config

--- sample_ball.cpp
+++ sample_ball.cpp
@@ -96,3 +96,16 @@ public:
 		unsigned int last_frame = SDL_GetTicks();
 		unsigned int frame_number = 0;
 		while(1){
+			//cout << "Frame " << frame_number << endl;
+			unsigned int current_frame = SDL_GetTicks();
+			unsigned int delta_ms = current_frame - last_frame;
+			
+			SDL_Event e;
+			//Handle all queued events
+			while(SDL_PollEvent(&e)){
+				switch(e.type){
+					case SDL_QUIT:
+						//Exit immediately
+						return;
+					case SDL_KEYDOWN:
+						//e.key stores the key pressed
