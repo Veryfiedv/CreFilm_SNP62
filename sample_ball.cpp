8006b11920d7fa1861b979c9cce2f196038cadc6
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-02-22 19:32:16 +0000

    de

--- sample_ball.cpp
+++ sample_ball.cpp
@@ -549,3 +549,18 @@ private:
 				int i=15*UNIT_X;
 				SDL_Rect TargetRect={i,j-20,50,50};
 				SDL_RenderCopy(renderer,plant1,&SrcRect1,&TargetRect);
+				if(start_back==false){
+						for(int m=i;m<i+UNIT_X;m++)
+							for(int n=j;n<j+UNIT_Y;n++)
+								box[m][n]=false;
+				}
+			}
+			for(int j=UNIT_Y;j<12*UNIT_Y;j=j+UNIT_Y){
+				int i=0;
+				SDL_Rect TargetRect={i,j-20,50,50};
+				SDL_RenderCopy(renderer,plant1,&SrcRect1,&TargetRect);
+				if(start_back==false){
+						for(int m=i;m<i+UNIT_X;m++)
+							for(int n=j;n<j+UNIT_Y;n++)
+								box[m][n]=false;
+				}
