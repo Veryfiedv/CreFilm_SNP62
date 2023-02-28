8f4deaf4f2c61e50c31f1caa4c7cf9ab57446d0c
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-02-28 16:32:24 +0000

    Change label

--- sample_ball.cpp
+++ sample_ball.cpp
@@ -590,3 +590,15 @@ private:
 			for(int i=2*UNIT_X;i<5*UNIT_X;i=i+UNIT_X)
 				for(int j=9*UNIT_Y;j<11*UNIT_Y;j=j+UNIT_Y){
 				SDL_Rect TargetRect={i,j-20,50,50};
+				SDL_RenderCopy(renderer,plant3,&SrcRect1,&TargetRect);
+				if(start_back==false){
+						for(int m=i;m<i+UNIT_X;m++)
+							for(int n=j;n<j+UNIT_Y;n++)
+								box[m][n]=false;
+				}
+			}
+			for(int i=13*UNIT_X;i<15*UNIT_X;i=i+UNIT_X){
+				int j=17*UNIT_Y;
+				SDL_Rect TargetRect={i,j-20,50,50};
+				SDL_RenderCopy(renderer,stone1,&SrcRect1,&TargetRect);
+				if(start_back==false){
