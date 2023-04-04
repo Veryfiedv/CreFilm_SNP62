b57e8573d9e997ecc754b0b244afefacd5c7629b
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-04-04 19:21:02 +0000

    bool improvements

--- vector2d.h
+++ vector2d.h
@@ -6,3 +6,14 @@
 
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
+
+#include <iostream>
+#include <cmath>
+
+class Vector2d{
+public:
+	Vector2d(){
+		this->x = this->y = 0;
+	}
+	Vector2d(float x, float y){
+			this->x = x;
