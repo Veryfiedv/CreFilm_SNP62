cdbbca51d523f4134a6e2a44b71f1a16933ac8fb
Veryfiedv <122662976+Veryfiedv@users.noreply.github.com>
2023-04-08 06:56:34 +0000

    W.I.P

--- vector2d.h
+++ vector2d.h
@@ -17,3 +17,12 @@ public:
 	}
 	Vector2d(float x, float y){
 			this->x = x;
+			this->y = y;
+	}
+	//No need to define copy constructors, since the default ones will suffice.
+	
+	Vector2d operator + (const Vector2d& other){
+		return Vector2d(x+other.x, y+other.y);
+	}
+	//Unary negation
+	Vector2d operator-(){
